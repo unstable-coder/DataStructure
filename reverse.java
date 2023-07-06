34b2512249e32f3a78ea9b50e76d97d6bc14dbc0
Raushan Ranjan <61554541+unstable-coder@users.noreply.github.com>
2023-07-06 18:19:28 +0530

    reverse.java

--- reverse.java
+++ reverse.java
@@ -1,1 +1,20 @@
-
+import java.util.*;
+class reverse{
+ 
+    static int[] rev(int[] arr , int n){
+        int i=0;
+        int j= arr.length-1;
+        while(i<j){
+           int temp = arr[i];
+           arr[i] = arr[j];
+           arr[j] = temp;
+           i++;
+           j--;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+      int[] arr = {3,5,3,2,6,8,8};
+      System.out.print(Arrays.toString(rev(arr, arr.length)));
+    }
+}
